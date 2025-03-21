45861d128d62997c3bac12bdcb215ac4de46173a
tttkiller <ishan.jain.cd.civ21@itbhu.ac.in>
2025-03-21 16:31:21 +0530

    Fixed mobile login issues

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: {
+    origin: "https://chatapp-4-lmax.onrender.com",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(express.json());
